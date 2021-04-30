fe6a925cb3dd3117880f1ff112c6f6c87cad94e9
Sydney <sydney@pugh.us.com>
2021-04-29 23:12:18 -0400

    unnecessary changes

--- profile_photo_verification/get_models.sh
+++ profile_photo_verification/get_models.sh
@@ -1,6 +1,10 @@
 OPENPOSE_URL="http://posefs1.perception.cs.cmu.edu/OpenPose/models/"
-POSE_FOLDER="pose/"
 FACE_FOLDER="face/"
+POSE_FOLDER="pose/"
+
+FACE_MODEL=${FACE_FOLDER}"pose_iter_116000.caffemodel"
+wget -c ${OPENPOSE_URL}${FACE_MODEL} -P ${FACE_FOLDER}
+wget -c "https://raw.githubusercontent.com/CMU-Perceptual-Computing-Lab/openpose/master/models/face/pose_deploy.prototxt" -P ${FACE_FOLDER}
 
 MPI_FOLDER=${POSE_FOLDER}"mpi/"
 MPI_MODEL=${MPI_FOLDER}"pose_iter_160000.caffemodel"
@@ -11,7 +15,3 @@ COCO_FOLDER=${POSE_FOLDER}"coco/"
 COCO_MODEL=${COCO_FOLDER}"pose_iter_440000.caffemodel"
 wget -c ${OPENPOSE_URL}${COCO_MODEL} -P ${COCO_FOLDER}
 wget -c "https://raw.githubusercontent.com/CMU-Perceptual-Computing-Lab/openpose/master/models/pose/coco/pose_deploy_linevec.prototxt" -P ${COCO_FOLDER}
-
-FACE_MODEL=${FACE_FOLDER}"pose_iter_116000.caffemodel"
-wget -c ${OPENPOSE_URL}${FACE_MODEL} -P ${FACE_FOLDER}
-wget -c "https://raw.githubusercontent.com/CMU-Perceptual-Computing-Lab/openpose/master/models/face/pose_deploy.prototxt" -P ${FACE_FOLDER}
